90a254aa18d649a6012ae06702333fd7c35e10f3
maura-aguilar <maurasol.aguilarandia@gmail.com>
2026-03-20 13:10:08 -0400

    2026-03-20_01 Programa de adivinanza en C++

---
+++ juegoadivinanza.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main (){
+std::cout << "**************************************" << std::endl ;
+std::cout << "Bienvenido al Juego de la ADIVINAZA!" << std::endl;
+std::cout << "**************************************" << std::endl;
+}
